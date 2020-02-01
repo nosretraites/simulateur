bbd3a35e853d8f6fe1d01c00690f8dfbcdd80513
Thomas Guillet <guillet.thomas@gmail.com>
2020-02-01 16:56:32 +0100

    Récupère des logs pour le déploiement continu

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ var ghpages = require('gh-pages')
 
 ghpages.publish('out', {
   repo: 'https://' + process.env.GH_TOKEN + '@github.com/nosretraites/simulateur.git',
-  silent: true
+//  silent: true
 }, (err) => {
   console.error("Error")
   console.log(err)
