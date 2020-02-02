6b72b3f996164bb231ab2a1407478678c9819924
Thomas Guillet <guillet.thomas@gmail.com>
2020-02-02 13:52:55 +0100

    Externalise la création du graphique

--- pages/poc.js
+++ pages/poc.js
@@ -9,6 +9,76 @@ import { fetchCareers, postSimpleForm } from "../services/api";
 
 import { withProvider } from "../components/Layout";
 
+const ResponsiveLineChart = ({data}) => (
+  <ResponsiveLine
+    data={data}
+    enablePoints={false}
+    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+    xScale={{ type: "linear" }}
+    yScale={{
+      type: "linear",
+      min: "auto",
+      max: "auto",
+      stacked: true,
+      reverse: false
+    }}
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      orient: "bottom",
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "Âge",
+      legendOffset: 36,
+      legendPosition: "middle"
+    }}
+    axisLeft={{
+      orient: "left",
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "Salaire",
+      legendOffset: -40,
+      legendPosition: "middle"
+    }}
+    colors={{ scheme: "nivo" }}
+    pointSize={10}
+    pointColor={{ theme: "background" }}
+    pointBorderWidth={2}
+    pointBorderColor={{ from: "serieColor" }}
+    pointLabel="y"
+    pointLabelYOffset={-12}
+    useMesh={true}
+    legends={[
+      {
+        anchor: "bottom-right",
+        direction: "column",
+        justify: false,
+        translateX: 100,
+        translateY: 0,
+        itemsSpacing: 0,
+        itemDirection: "left-to-right",
+        itemWidth: 80,
+        itemHeight: 20,
+        itemOpacity: 0.75,
+        symbolSize: 12,
+        symbolShape: "circle",
+        symbolBorderColor: "rgba(0, 0, 0, .5)",
+        effects: [
+          {
+            on: "hover",
+            style: {
+              itemBackground: "rgba(0, 0, 0, .03)",
+              itemOpacity: 1
+            }
+          }
+        ]
+      }
+    ]}
+  />
+)
+
 const SimpleForm = () => {
   const [raw, setRaw] = useState(false);
   const [salaryPlotData, setSalaryPlotData] = useState([]);
@@ -30,77 +100,10 @@ const SimpleForm = () => {
     })
   }, []);
 
-
   return (
     <div style={{ height: "80vh" }}>
       <h1>TEST</h1>
-            <ResponsiveLine
-        data={salaryPlotData}
-        enablePoints={false}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: "linear" }}
-        yScale={{
-          type: "linear",
-          min: "auto",
-          max: "auto",
-          stacked: true,
-          reverse: false
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          orient: "bottom",
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "Âge",
-          legendOffset: 36,
-          legendPosition: "middle"
-        }}
-        axisLeft={{
-          orient: "left",
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "Salaire",
-          legendOffset: -40,
-          legendPosition: "middle"
-        }}
-        colors={{ scheme: "nivo" }}
-        pointSize={10}
-        pointColor={{ theme: "background" }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: "serieColor" }}
-        pointLabel="y"
-        pointLabelYOffset={-12}
-        useMesh={true}
-        legends={[
-          {
-            anchor: "bottom-right",
-            direction: "column",
-            justify: false,
-            translateX: 100,
-            translateY: 0,
-            itemsSpacing: 0,
-            itemDirection: "left-to-right",
-            itemWidth: 80,
-            itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: "circle",
-            symbolBorderColor: "rgba(0, 0, 0, .5)",
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemBackground: "rgba(0, 0, 0, .03)",
-                  itemOpacity: 1
-                }
-              }
-            ]
-          }
-        ]}
-      />
+      <ResponsiveLineChart data={salaryPlotData} />
     </div>
   );
 };
