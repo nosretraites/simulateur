0f48637061986f1c5f05ba7bd69564045dd81253
Charles <charles.jacquin@autistici.org>
2020-02-02 14:27:21 +0100

    fix context

--- context/index.js
+++ context/index.js
@@ -2,34 +2,9 @@ import React, { createContext, useState } from 'react';
 
 const Context = createContext({});
 
-// class Provider extends React.Component {
-//   state = {
-//     simpleResult: {},
-//     updateData: this.handleUpdateData.bind(this)
-//   }
-
-//   handleUpdateData(data) {
-//     this.setState({
-//       simpleResult: data
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <Context.Provider value={{
-//         simpleResult,
-//         updateData
-//       }}>
-//         {children}
-//       </Context.Provider>
-//     );
-//   }
-// }
-
 const Provider = ({ children }) => {
   const [simpleResult, updateData] = useState({});
 
-  console.log('context, ', simpleResult);
   return (
     <Context.Provider value={{
       simpleResult,
